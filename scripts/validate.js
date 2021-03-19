09f2d95a383f3fc136a0054a2dcb7698d4a869eb
julyanazar <nnwrnuwr@gmail,com>
2021-03-19 16:06:57 +0400

    feat: заготовка для валидации форм

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+enableValidation({
+    formSelector: '.form',
+    inputSelector: 'form__input',
+    submitButtonSelector: '.form__save-button',
+    inactiveButtonClass: 'form__save-button_invalid',
+    inputErrorClass: 'form__input_type_error',
+    errorClass: 'form__error_visible'
+  }); 
